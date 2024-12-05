423d86f4038d3894cbfd1f7942a837b5435bacf9
Pedro Davi <160784736+pedrodavi120@users.noreply.github.com>
2024-12-05 19:07:18 +0000

    Fiz a sessão da pagina inicial e adicionei a imagem.

--- src/paginas/PaginaPrincipal.tsx
+++ src/paginas/PaginaPrincipal.tsx
@@ -1,4 +1,18 @@
 import React from "react";
-export default function PaginaPrincipal () {
+import imagem from "../assets/mesadecomidasPG.png";
+import "./PaginaPrincipal.css";
 
+export default function PaginaPrincipal () { 
+  return ( 
+    <div id ="BodyPI"> 
+      <h1>De La Cocina</h1> 
+      <br />
+      <p>Descubre el rincón donde la pasión por la cocina argentina cobra vida:</p> <p>recetas fáciles, auténticas y llenas de sabor para transformar cada plato</p> <p>en una celebración.</p>  
+    <br />
+      <button id="BotaoSobrePI">Sobre nosotros</button> 
+      <br />
+      <br />
+      <img id= "ImagemPI"src={imagem} alt="Imagem da mesa de comidas" />
+    </div> 
+  ); 
 }
