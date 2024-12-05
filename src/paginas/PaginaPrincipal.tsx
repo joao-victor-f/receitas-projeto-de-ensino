b4964395afdc0fe6afec656aa4e35ffa07f3f9b5
Pedro Davi <160784736+pedrodavi120@users.noreply.github.com>
2024-12-05 20:50:53 +0000

    Terminei as duas sessões da pagina inicial.

--- src/paginas/PaginaPrincipal.tsx
+++ src/paginas/PaginaPrincipal.tsx
@@ -9,7 +9,7 @@ export default function PaginaPrincipal () {
     <section id ="BodyPI"> 
       <h1>De La Cocina</h1> 
       <br />
-      <p>Descubre el rincón donde la pasión por la cocina argentina cobra vida:</p> <p>recetas fáciles, auténticas y llenas de sabor para transformar cada plato</p> <p>en una celebración.</p>  
+      <p>Descubre el rincón donde la pasión por la cocina argentina cobra vida:recetas fáciles, auténticas y llenas de sabor para transformar cada plato en una</p><p> celebración.</p>  
     <br />
       <button id="BotaoSobrePI">Sobre nosotros</button> 
       <br />
@@ -18,12 +18,14 @@ export default function PaginaPrincipal () {
     </section>
     
     <section id= "ContS2">
+      <div id= "textoS2">
     <h1>Disfruta la cocina argentina</h1>
-    <p>Descubre los sabores únicos de Argentina con nuestras recetas tradicionales y modernas. Desde el clásico asado hasta las deliciosas empanadas, explora lo mejor de nuestra gastronomía.</p>
+    <p>Descubre los sabores únicos de Argentina con nuestras recetas</p> <p>tradicionales y modernas. Desde el clásico asado hasta las</p><p>deliciosas empanadas, explora lo mejor de nuestra gastronomía.</p>
     <h2>Dulzura argentina: Postres que conquistan</h2>
-    <p>¿Te apasionan los postres? Aprende a preparar alfajores, flanes y otros dulces típicos que reflejan nuestra rica historia culinaria.</p>
+    <p>¿Te apasionan los postres? Aprende a preparar alfajores, flanes</p> <p>y otros dulces típicos que reflejan nuestra rica historia culinaria.</p>
     <h2>Sabores regionales que cuentan historias</h2>
-    <p>Sumérgete en la cocina regional y conoce los secretos detrás de platos como el locro, las humitas y los tamales, herencia de nuestras raíces indígenas y criollas.</p>
+    <p>Sumérgete en la cocina regional y conoce los secretos detrás de </p><p> platos como el locro, las humitas y los tamales, herencia de </p> <p>nuestras raíces indígenas y criollas.</p>
+    </div>
         <div id="image-container">
         <img id= "ImagemPI2" src={imagem2} alt="Imagen de comida argentina" />
       </div>
