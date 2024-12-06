07395ee611d54f37bdd6c815aac461a5e6b5e1cb
victor <joaovicif2021@gmail.com>
2024-12-05 21:54:35 -0300

    adicionada parte da história da cozinha

--- src/paginas/categorias/Categorias.tsx
+++ src/paginas/categorias/Categorias.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
-export default function Categorias () {
+import './categorias.css';
+import ListagemDeCategorias from '../../components/listagemdereceitas/ListagemDeCategorias';
+
+export default function Categorias() {
     return (
-        <h1>Categorias</h1>
-    );
+        <>
+            <section id="intro-receitas">
+                <div id="title-receitas">
+                    <h1>Recetas</h1>
+                    <span>El sabor de Argentina a un clic: recetas, paso a paso y mucha inspiracion para tu cocina.</span>
+                </div>
+                <div>
+                    <a href="#">+ AGREGAR RECETA</a>
+                </div>
+            </section>
+            <ListagemDeCategorias></ListagemDeCategorias>
+            <section id="historia-cozinha">
+                <div id="images-container">
+                    <img src="/categorias/img-suporte-2.png" alt="" />
+                    <img src="/categorias/img-suporte-1.png" alt="" />
+                </div>
+                <div id="historia-container">
+                    <h1>Historia de la cocina argentina</h1>
+                    <p>La historia de la cocina argentina es un reflejo de su diversidad cultural y geográfica. Influenciada por las tradiciones indígenas, como las de los pueblos guaraníes y mapuches, y enriquecida con la llegada de inmigrantes europeos, especialmente italianos y españoles, la gastronomía argentina combina sabores y técnicas únicas.</p>
+                    <p>El asado, símbolo nacional, tiene raíces en la vida de los gauchos, quienes perfeccionaron la tradición de cocinar carnes al aire libre. A esto se suman platos emblemáticos como las empanadas, el locro y la milanesa, así como la influencia italiana en pastas y pizzas.</p>
+                    <p>Además, la producción de vinos, especialmente en Mendoza, y el consumo de mate reflejan la conexión del país con su tierra y tradiciones. La cocina argentina sigue evolucionando, mezclando lo tradicional con la innovación, pero siempre conservando su identidad única.</p>
+                </div>
+            </section>
+        </>
+    )
 }
--- src/paginas/receitas/Receitas.tsx
+++ src/paginas/receitas/Receitas.tsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import './receitas.css';
-import ListagemDeReceitas from '../../components/listagemdereceitas/ListagemDeCategorias';
-
-export default function Receitas() {
+export default function Categorias () {
     return (
-        <>
-            <section id="intro-receitas">
-                <div id="title-receitas">
-                    <h1>Recetas</h1>
-                    <span>El sabor de Argentina a un clic: recetas, paso a paso y mucha inspiracion para tu cocina.</span>
-                </div>
-                <div>
-                    <a href="#">+ AGREGAR RECETA</a>
-                </div>
-            </section>
-            <ListagemDeReceitas></ListagemDeReceitas>
-        </>
-    )
+        <h1>Categorias</h1>
+    );
 }
