e09e59e6abf656ea42a843c8f85bb7bd48443022
victor <joaovicif2021@gmail.com>
2024-12-05 22:51:43 -0300

    adicionada página de categorias

--- src/paginas/categorias/Categorias.tsx
+++ src/paginas/categorias/Categorias.tsx
@@ -1,6 +1,8 @@
+import { data } from '../../lib/data';
 import React from 'react';
 import './categorias.css';
 import ListagemDeCategorias from '../../components/listagemdereceitas/ListagemDeCategorias';
+import Receita from '../../components/receita/Receita';
 
 export default function Categorias() {
     return (
@@ -25,6 +27,14 @@ export default function Categorias() {
                     <p>La historia de la cocina argentina es un reflejo de su diversidad cultural y geográfica. Influenciada por las tradiciones indígenas, como las de los pueblos guaraníes y mapuches, y enriquecida con la llegada de inmigrantes europeos, especialmente italianos y españoles, la gastronomía argentina combina sabores y técnicas únicas.</p>
                     <p>El asado, símbolo nacional, tiene raíces en la vida de los gauchos, quienes perfeccionaron la tradición de cocinar carnes al aire libre. A esto se suman platos emblemáticos como las empanadas, el locro y la milanesa, así como la influencia italiana en pastas y pizzas.</p>
                     <p>Además, la producción de vinos, especialmente en Mendoza, y el consumo de mate reflejan la conexión del país con su tierra y tradiciones. La cocina argentina sigue evolucionando, mezclando lo tradicional con la innovación, pero siempre conservando su identidad única.</p>
+                </div>
+            </section>
+            <section id="recomendacao-receitas">
+                <h1>Recomendacion de recetas</h1>
+                <div className="receitas-container">
+                    <Receita receita={data.receitas[0]} />
+                    <Receita receita={data.receitas[1]} />
+                    <Receita receita={data.receitas[2]} />
                 </div>
             </section>
         </>
