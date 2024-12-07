7ab8b75faf232eb5db761253e732c1db7bc93438
José Vine <95425180+jose-vine@users.noreply.github.com>
2024-12-07 00:05:32 +0000

    Esboço da página igredientes

--- src/paginas/ingredientes/Ingredientes.tsx
+++ src/paginas/ingredientes/Ingredientes.tsx
@@ -1,6 +1,56 @@
 import React from 'react';
+import styles from './ingredientes.module.css';
+import alfajor from '../../assets/alfajor.jpg';
+
 export default function Ingredientes() {
     return (
-        <h1>Ingredientes</h1>
+        <>
+            <h1>Ingredientes</h1>
+            <span>Los ingredientes que componen la cocina argentina</span>
+            <span>+ AGREGAR INGREDIENTE</span>
+
+            <div className={styles.fileiraDeReceitas}>
+                <div className={styles.receita} id={styles.receitaUm}>
+                    <img src={alfajor} alt="Alfajor" />
+                    <p>Alfajor</p>
+                </div>
+                <div className={styles.receita} id={styles.receitaDois}>
+                    <img src={alfajor} alt="Alfajor" />
+                    <p>Alfajor</p>
+                </div>
+                <div className={styles.receita} id={styles.receitaTres}>
+                    <img src={alfajor} alt="Alfajor" />
+                    <p>Alfajor</p>
+                </div>
+            </div>
+            <div className={styles.fileiraDeReceitas}>
+                <div className={styles.receita} id={styles.receitaUm}>
+                    <img src={alfajor} alt="Alfajor" />
+                    <p>Alfajor</p>
+                </div>
+                <div className={styles.receita} id={styles.receitaDois}>
+                    <img src={alfajor} alt="Alfajor" />
+                    <p>Alfajor</p>
+                </div>
+                <div className={styles.receita} id={styles.receitaTres}>
+                    <img src={alfajor} alt="Alfajor" />
+                    <p>Alfajor</p>
+                </div>
+            </div>
+            <div className={styles.fileiraDeReceitas}>
+                <div className={styles.receita} id={styles.receitaUm}>
+                    <img src={alfajor} alt="Alfajor" />
+                    <p>Alfajor</p>
+                </div>
+                <div className={styles.receita} id={styles.receitaDois}>
+                    <img src={alfajor} alt="Alfajor" />
+                    <p>Alfajor</p>
+                </div>
+                <div className={styles.receita} id={styles.receitaTres}>
+                    <img src={alfajor} alt="Alfajor" />
+                    <p>Alfajor</p>
+                </div>
+            </div>
+        </>
     );
 }
