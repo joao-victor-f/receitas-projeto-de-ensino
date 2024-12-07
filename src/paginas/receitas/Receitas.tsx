2e3eb5e9d299a91d19b785463e17f43ada1a1ba5
victor <joaovicif2021@gmail.com>
2024-12-06 21:46:45 -0300

    feita a lógica de dinamicidade da página

--- src/paginas/receitas/Receitas.tsx
+++ src/paginas/receitas/Receitas.tsx
@@ -1,6 +1,42 @@
-import React from 'react';
-export default function Categorias () {
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { data } from '../../lib/data';
+import { Link } from 'react-router-dom';
+import Receita from '../../components/receita/Receita';
+export default function Receitas() {
+    const location = useLocation();
+    const [categoria, setCategoria] = useState<Categoria | undefined>(undefined);
+    const [receitas, setReceitas] = useState<Receitas[] | undefined>([]);
+
+
+
+    useEffect(() => {
+        const currentPath = location.pathname;
+        console.log(currentPath);
+        const categoriaEncontrada = data.categorias.find((categoria) => categoria.path == currentPath);
+        setCategoria(categoriaEncontrada);
+
+        if (!categoriaEncontrada) {
+            return;
+        }
+        const receitasEncontradas = data.receitas.filter((receita) => receita.categoria.titulo == categoriaEncontrada.titulo);
+        if (!receitasEncontradas) {
+            return;
+        }
+        setReceitas(receitasEncontradas);
+        console.log(receitasEncontradas);
+    }, [setCategoria, setReceitas])
     return (
-        <h1>Categorias</h1>
+        <section>
+            {(receitas && receitas.length > 0) && 
+                receitas.map(receita => {
+                    return (
+                        <Link to={receita.path}>
+                            <Receita receita={receita}></Receita>
+                        </Link>
+                    )
+                })
+            }
+        </section>
     );
 }
