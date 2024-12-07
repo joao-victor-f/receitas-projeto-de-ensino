1da3b43849102dd89eb0a42ca91466e062caabc3
victor <joaovicif2021@gmail.com>
2024-12-06 21:48:14 -0300

    feitas alterações nos dados para se adequar aos tipos

---
+++ src/lib/types.ts
@@ -0,0 +1,27 @@
+type Categoria = {
+    titulo: string,
+    imagemURL: string,
+    descricao: string,
+    path: string,
+    fullImagemURL: string,
+};
+
+type Ingrediente = {
+    nome: string,
+    imagemURL: string,
+};
+
+type Receitas = {
+    nome: string,
+    categoria: Categoria,
+    descricao: string,
+    ingredientes: {
+        ingrediente: Ingrediente,
+        quantidade: number,
+        medicao: string,
+    }[],
+    imagemURL: string,
+    tempoDePreparacao: string,
+    passos: string[],
+    path: string,
+};
