c3f16f5be2273a4ba166b27109b01a12510be195
José Vine <95425180+jose-vine@users.noreply.github.com>
2024-12-07 00:51:43 +0000

    Conclusão da parte das receitas v2

--- src/paginas/ingredientes/Ingredientes.tsx
+++ src/paginas/ingredientes/Ingredientes.tsx
@@ -9,46 +9,48 @@ export default function Ingredientes() {
             <span>Los ingredientes que componen la cocina argentina</span>
             <span>+ AGREGAR INGREDIENTE</span>
 
-            <div className={styles.fileiraDeReceitas}>
-                <div className={styles.receita} id={styles.receitaUm}>
-                    <img src={alfajor} alt="Alfajor" />
-                    <p>Alfajor</p>
+            <div id={styles.grupoReceitas}>
+                <div className={styles.fileiraDeReceitas} id={styles.fileiraDeReceitasUm}>
+                    <div className={styles.receita} id={styles.receitaUm}>
+                        <img src={alfajor} alt="Alfajor" className={styles.imagemReceita}/>
+                        <p className={styles.nomeReceita}>Alfajor</p>
+                    </div>
+                    <div className={styles.receita} id={styles.receitaDois}>
+                        <img src={alfajor} alt="Alfajor" className={styles.imagemReceita}/>
+                        <p className={styles.nomeReceita}>Alfajor</p>
+                    </div>
+                    <div className={styles.receita} id={styles.receitaTres}>
+                        <img src={alfajor} alt="Alfajor" className={styles.imagemReceita}/>
+                        <p className={styles.nomeReceita}>Alfajor</p>
+                    </div>
                 </div>
-                <div className={styles.receita} id={styles.receitaDois}>
-                    <img src={alfajor} alt="Alfajor" />
-                    <p>Alfajor</p>
+                <div className={styles.fileiraDeReceitas} id={styles.fileiraDeReceitasDois}>
+                    <div className={styles.receita} id={styles.receitaQuatro}>
+                        <img src={alfajor} alt="Alfajor" className={styles.imagemReceita}/>
+                        <p className={styles.nomeReceita}>Alfajor</p>
+                    </div>
+                    <div className={styles.receita} id={styles.receitaCinco}>
+                        <img src={alfajor} alt="Alfajor" className={styles.imagemReceita}/>
+                        <p className={styles.nomeReceita}>Alfajor</p>
+                    </div>
+                    <div className={styles.receita} id={styles.receitaSeis}>
+                        <img src={alfajor} alt="Alfajor" className={styles.imagemReceita}/>
+                        <p className={styles.nomeReceita}>Alfajor</p>
+                    </div>
                 </div>
-                <div className={styles.receita} id={styles.receitaTres}>
-                    <img src={alfajor} alt="Alfajor" />
-                    <p>Alfajor</p>
-                </div>
-            </div>
-            <div className={styles.fileiraDeReceitas}>
-                <div className={styles.receita} id={styles.receitaUm}>
-                    <img src={alfajor} alt="Alfajor" />
-                    <p>Alfajor</p>
-                </div>
-                <div className={styles.receita} id={styles.receitaDois}>
-                    <img src={alfajor} alt="Alfajor" />
-                    <p>Alfajor</p>
-                </div>
-                <div className={styles.receita} id={styles.receitaTres}>
-                    <img src={alfajor} alt="Alfajor" />
-                    <p>Alfajor</p>
-                </div>
-            </div>
-            <div className={styles.fileiraDeReceitas}>
-                <div className={styles.receita} id={styles.receitaUm}>
-                    <img src={alfajor} alt="Alfajor" />
-                    <p>Alfajor</p>
-                </div>
-                <div className={styles.receita} id={styles.receitaDois}>
-                    <img src={alfajor} alt="Alfajor" />
-                    <p>Alfajor</p>
-                </div>
-                <div className={styles.receita} id={styles.receitaTres}>
-                    <img src={alfajor} alt="Alfajor" />
-                    <p>Alfajor</p>
+                <div className={styles.fileiraDeReceitas} id={styles.fileiraDeReceitasTres}>
+                    <div className={styles.receita} id={styles.receitaSete}>
+                        <img src={alfajor} alt="Alfajor" className={styles.imagemReceita}/>
+                        <p className={styles.nomeReceita}>Alfajor</p>
+                    </div>
+                    <div className={styles.receita} id={styles.receitaOito}>
+                        <img src={alfajor} alt="Alfajor" className={styles.imagemReceita}/>
+                        <p className={styles.nomeReceita}>Alfajor</p>
+                    </div>
+                    <div className={styles.receita} id={styles.receitaNove}>
+                        <img src={alfajor} alt="Alfajor" className={styles.imagemReceita}/>
+                        <p className={styles.nomeReceita}>Alfajor</p>
+                    </div>
                 </div>
             </div>
         </>
