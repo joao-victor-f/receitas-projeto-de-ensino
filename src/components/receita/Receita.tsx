0671990cfbf8920b0297a403bde775041a6a77cf
victor <joaovicif2021@gmail.com>
2024-12-08 01:11:08 -0300

    modificações no componente de receita

--- src/components/receita/Receita.tsx
+++ src/components/receita/Receita.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import styles from "./receita.module.css";
+import { formatarTempo } from '../../lib/utils';
+import { Link } from 'react-router-dom';
+
 export default function Receita(props) {
     return (
         <article className={styles.receita}>
-            <img src={props.receita.imagemURL}/>
-            <span>{props.receita.nome}</span>
+            <div className={styles.receita_container}>
+                <img src={props.receita.imagemURL}/>
+                <span>{props.receita.nome}</span>
+            </div>
+            <div className={styles.info_container}>
+                <div className={styles.ingredientes_container}>
+                    <img src="/icons/ingredientes.svg" />
+                    <span>{`${props.receita.ingredientes.length} ingredientes`}</span>
+                </div>
+                <div className={styles.tempo_container}>
+                    <img src="/icons/relogio.svg" />
+                    <span>{`${formatarTempo(props.receita.tempoDePreparacao)}`}</span>
+                </div>
+            </div>
+            <div className={styles.cozinhar_container}>
+                <a>Cocinar</a>
+                <img src="/icons/panela.svg" />
+            </div>
         </article>
     );
 }
