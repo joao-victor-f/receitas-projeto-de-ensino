69e774a9a325b126c3c37d4f74d6d6ef1349e9bc
Pedro Davi <160784736+pedrodavi120@users.noreply.github.com>
2024-12-08 21:39:27 +0000

    Fiz o código da página em React e o CSS está quase pronto... agora é só adicionar os assets dos elementos.

--- src/paginas/CriarReceitas/Criar-receitas.tsx
+++ src/paginas/CriarReceitas/Criar-receitas.tsx
@@ -1,98 +1,245 @@
 import React from "react";
+import { AddAPhotoButton } from "./AddAPhotoButton";
+import { AddButton } from "./AddButton";
+import { CuisineDropdown } from "./CuisineDropdown";
+import { IngredientListing } from "./IngredientListing";
+import { InputFieldWoLabel } from "./InputFieldWoLabel";
+import { Label } from "./Label";
+import { RemoveButton } from "./RemoveButton";
+import { StepsAddStep } from "./StepsAddStep";
+import divider2 from "./divider-2.svg";
+import divider3 from "./divider-3.svg";
+import divider4 from "./divider-4.svg";
+import divider5 from "./divider-5.svg";
+import divider from "./divider.svg";
+import image from "./image.svg";
 import "./criar-receitas.module.css";
 
-export default function CriarReceitas () {
+export const CriarReceitas = () => {
     return (
-        <div id="formulario-adicionar-receita">
-            <div id="entradas-receita">
-                <div id="foto-receita">
-                    <div id="titulo-adicionar-receita">Agregar una receta</div>
-                    <p id="descricao-receita">
-                        ¿Te sientes como un Picasso de cocina? ¡Queremos ver tu obra maestra! Añade tu receta y muestra tu
-                        creatividad culinaria.
+        <div className="add-recipe-form">
+            <div className="add-recipe-inputs">
+                <div className="add-a-recipe-photo">
+                    <div className="text-wrapper-2">Agregar una receta</div>
+
+                    <p className="p">
+                        ¿Te sientes como un Picasso de cocina? ¡Queremos ver tu obra
+                        maestra! Añade tu receta y muestra tu creatividad culinaria.
                     </p>
-                    <div id="enviar-imagem">
-                        <div id="botao-adicionar-foto">
-                            <div id="icone-foto"><img id="imagem-vetor" src="img/vector-6.svg" /></div>
-                            <div id="texto-adicionar-foto">Añadir foto</div>
+
+                    <div className="upload-image">
+                        <AddAPhotoButton
+                            className="add-a-photo-button-instance"
+                            groupClassName="add-a-photo-button-2"
+                            property1="default"
+                            text="Añadir foto"
+                            vector="vector-6-3.svg"
+                        />
+                    </div>
+
+                    <img className="divider" alt="Divider" src={divider} />
+
+                    <img className="img" alt="Divider" src={image} />
+
+                    <img className="divider-2" alt="Divider" src={divider2} />
+
+                    <div className="servings-prep-time">
+                        <div className="title-input">
+                            <Label
+                                className="design-component-instance-node-3"
+                                property1="required"
+                                text="Porciones"
+                            />
+                            <InputFieldWoLabel
+                                className="input-field-wo-label-5"
+                                divClassName="input-field-wo-label-6"
+                                inputText="e.g., 4"
+                                property1="default"
+                            />
+                        </div>
+
+                        <div className="prep-time-input">
+                            <div className="label-wrapper">
+                                <Label
+                                    className="design-component-instance-node-3"
+                                    property1="required"
+                                    text="Tiempo de preparación"
+                                />
+                            </div>
+
+                            <div className="hrs-mins">
+                                <InputFieldWoLabel
+                                    className="input-field-wo-label-7"
+                                    divClassName="input-field-wo-label-8"
+                                    inputText="hrs"
+                                    property1="default"
+                                />
+                                <InputFieldWoLabel
+                                    className="input-field-wo-label-7"
+                                    divClassName="input-field-wo-label-8"
+                                    inputText="mins"
+                                    property1="default"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="prep-time-input-2">
+                            <div className="label-wrapper">
+                                <Label
+                                    className="design-component-instance-node-3"
+                                    property1="required"
+                                    text="Tiempo de cocción"
+                                />
+                            </div>
+
+                            <div className="hrs-mins">
+                                <InputFieldWoLabel
+                                    className="input-field-wo-label-7"
+                                    divClassName="input-field-wo-label-8"
+                                    inputText="hrs"
+                                    property1="default"
+                                />
+                                <InputFieldWoLabel
+                                    className="input-field-wo-label-7"
+                                    divClassName="input-field-wo-label-8"
+                                    inputText="mins"
+                                    property1="default"
+                                />
+                            </div>
                         </div>
                     </div>
-                    <img id="divisor-principal" src="img/divider-2.svg" />
-                    <img id="divisor-secundario" src="img/divider.svg" />
-                    <img id="divisor-terciario" src="img/divider-4.svg" />
-                    <div id="porcoes-e-tempo">
-                        <div id="entrada-porcoes">
-                            <div id="rotulo-porcoes">
-                                <div id="texto-rotulo-porcoes">Porciones</div>
-                                <div id="asterisco-porcoes">*</div>
-                            </div>
-                            <div id="campo-entrada-porcoes">
-                                <div id="input-porcoes"><div id="texto-input-porcoes">e.g., 4</div></div>
-                            </div>
+
+                    <div className="recipe-title">
+                        <div className="title-input-2">
+                            <Label
+                                className="design-component-instance-node-3"
+                                property1="required"
+                                text="Título de la receta"
+                            />
+                            <InputFieldWoLabel
+                                className="input-field-wo-label-9"
+                                divClassName="input-field-wo-label-10"
+                                inputText="Introduce el título de tu receta"
+                                property1="default"
+                            />
                         </div>
-                        <div id="entrada-tempo-preparo">
-                            <div id="rotulo-tempo">
-                                <div id="texto-rotulo-tempo">Tiempo de preparación</div>
-                                <div id="asterisco-tempo">*</div>
-                            </div>
-                            <div id="campo-tempo">
-                                <div id="campo-horas">
-                                    <div id="input-horas"><div id="texto-input-horas">hrs</div></div>
-                                </div>
-                                <div id="campo-minutos">
-                                    <div id="input-minutos"><div id="texto-input-minutos">mins</div></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="entrada-tempo-cozimento">
-                            <div id="rotulo-cozimento">
-                                <div id="texto-rotulo-cozimento">Tiempo de cocción</div>
-                                <div id="asterisco-cozimento">*</div>
-                            </div>
-                            <div id="campo-cozimento">
-                                <div id="campo-horas-cozimento">
-                                    <div id="input-horas-cozimento"><div id="texto-input-horas-cozimento">hrs</div></div>
-                                </div>
-                                <div id="campo-minutos-cozimento">
-                                    <div id="input-minutos-cozimento"><div id="texto-input-minutos-cozimento">mins</div></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="titulo-descricao-receita">
-                        <div id="entrada-titulo">
-                            <div id="rotulo-titulo">
-                                <div id="texto-rotulo-titulo">Título de la receta</div>
-                                <div id="asterisco-titulo">*</div>
-                            </div>
-                            <div id="campo-titulo">
-                                <div id="input-titulo"><p id="texto-input-titulo">Introduce el título de tu receta</p></div>
-                            </div>
-                        </div>
-                        <div id="entrada-descricao">
-                            <div id="rotulo-descricao">
-                                <div id="texto-rotulo-descricao">Descripción</div>
-                                <div id="asterisco-descricao">*</div>
-                            </div>
-                            <div id="campo-descricao">
-                                <div id="input-descricao"><p id="texto-input-descricao">Describe tu receta de una manera que haga la boca agua.</p></div>
-                            </div>
+
+                        <div className="title-input-2">
+                            <Label
+                                className="design-component-instance-node-3"
+                                property1="required"
+                                text="Descripción"
+                            />
+                            <InputFieldWoLabel
+                                className="input-field-wo-label-11"
+                                divClassName="input-field-wo-label-10"
+                                inputText="Describe tu receta de una manera que haga la boca agua."
+                                property1="default"
+                            />
                         </div>
                     </div>
                 </div>
-                <div id="ingredientes">
-                    <div id="secao-ingredientes">
-                        <p id="texto-instrucao-ingredientes">
-                            Enumere un ingrediente por línea, especificando cantidades (1, 2), medidas (tazas, cucharas) y cualquier
-                            detalle de preparación (picado, tamizado) junto con el artículo.
-                        </p>
-                        <div id="rotulo-ingredientes">
-                            <div id="texto-rotulo-ingredientes">Ingredientes</div>
-                            <div id="asterisco-ingredientes">*</div>
+
+                <div className="ingredients">
+                    <div className="ingredients-2">
+                        <div className="ingredients-3">
+                            <p className="text-wrapper-3">
+                                Enumere un ingrediente por línea, especificando cantidades (1,
+                                2), medidas (tazas, cucharas) y cualquier detalle de preparación
+                                (picado, tamizado) junto con el artículo.
+                            </p>
+
+                            <Label
+                                className="label-instance"
+                                property1="required"
+                                text="Ingredientes"
+                            />
                         </div>
-                        <div id="lista-ingredientes">
+
+                        <IngredientListing
+                            addButtonText="+ Agregar ingrediente"
+                            className="design-component-instance-node-3"
+                            property1="four"
+                            reOrderListRemoveInputFieldWoLabelDivClassName="ingredient-listing-instance"
+                            reOrderListRemoveInputFieldWoLabelDivClassName1="ingredient-listing-instance"
+                            reOrderListRemoveInputFieldWoLabelDivClassName2="ingredient-listing-instance"
+                            reOrderListRemoveInputFieldWoLabelDivClassNameOverride="ingredient-listing-instance"
+                            reOrderListRemoveInputFieldWoLabelInputText="Cantidad"
+                            reOrderListRemoveInputFieldWoLabelInputText1="Producto"
+                            reOrderListRemoveInputFieldWoLabelInputText2="Cantidad"
+                            reOrderListRemoveInputFieldWoLabelInputText3="Producto"
+                            reOrderListRemoveInputFieldWoLabelInputText4="Cantidad"
+                            reOrderListRemoveInputFieldWoLabelInputText5="Producto"
+                            reOrderListRemoveInputFieldWoLabelInputText6="Cantidad"
+                            reOrderListRemoveInputFieldWoLabelInputText7="Producto"
+                            reOrderListRemoveMeasurementDropdownText="Medición"
+                            reOrderListRemoveMeasurementDropdownText1="Medición"
+                            reOrderListRemoveMeasurementDropdownText2="Medición"
+                            reOrderListRemoveMeasurementDropdownText3="Medición"
+                        />
+                    </div>
+
+                    <img className="divider-3" alt="Divider" src={divider3} />
+
+                    <div className="instructions">
+                        <div className="div-3">
+                            <Label
+                                className="design-component-instance-node-3"
+                                property1="required"
+                                text="Instrucciones"
+                            />
+                            <p className="text-wrapper-4">
+                                Divide tu receta en instrucciones claras paso a paso.
+                            </p>
+
+                            <AddButton
+                                className="add-button-3"
+                                divClassName="add-button-2"
+                                property1="default"
+                                text="+ Agregar video o imagen"
+                            />
                         </div>
-                        <div id="botao-adicionar-ingrediente"><div id="texto-adicionar-ingrediente">+ Agregar ingrediente</div></div>
+
+                        <StepsAddStep
+                            addButtonText="+ Agregar paso"
+                            className="design-component-instance-node-3"
+                            override={<RemoveButton className="remove-button-2" />}
+                            property1="two"
+                            stepDetailsIcon={<RemoveButton className="remove-button-2" />}
+                            stepDetailsInputFieldWoLabelInputText="Texto de entrada"
+                            stepDetailsInputFieldWoLabelInputText1="Texto de entrada"
+                            stepDetailsText="Paso 1"
+                            stepDetailsText1="Paso 2"
+                        />
+                    </div>
+
+                    <img className="divider-3" alt="Divider" src={divider4} />
+
+                    <div className="div-3">
+                        <Label
+                            className="design-component-instance-node-3"
+                            property1="optional"
+                            text="Consejos de cocina"
+                        />
+                        <InputFieldWoLabel
+                            className="input-field-wo-label-11"
+                            divClassName="input-field-wo-label-10"
+                            inputText="¡Comparte tus secretos de cocina! Trucos, intercambios o cualquier consejo sobre el horno para lograr el máximo éxito en la receta."
+                            property1="default"
+                        />
+                    </div>
+
+                    <img className="divider-3" alt="Divider" src={divider5} />
+
+                    <div className="tags">
+                        <Label
+                            className="design-component-instance-node-3"
+                            property1="optional"
+                            text="Categoría"
+                        />
+                        <div className="tag-options">
+                            <CuisineDropdown property1="default" text="Tipo de comida" />
+                        </div>
                     </div>
                 </div>
             </div>
