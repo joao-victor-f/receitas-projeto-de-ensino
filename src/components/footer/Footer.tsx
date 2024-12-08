012bd941105aa3a6894788cca4946020501d58af
victor <joaovicif2021@gmail.com>
2024-12-08 00:23:05 -0300

    alterações no footer para comportar o texto de copyrights

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,42 +6,45 @@ import { Link } from "react-router-dom";
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <div className={styles.container}>
-                <div className={styles.intro_container}>
-                    <Link to="/">
-                        <img src={logo} />
-                    </Link>
-                    <div className={styles.social_container}>
-                        <a href="">
-                            <img src="/icons/facebook-icon.svg" alt="ícone do facebook" />
-                        </a>
-                        <a href="">
-                            <img src="/icons/instagram-icon.png" alt="ícone do instagram" />
-                        </a>
-                        <a href="">
-                            <img src="/icons/tiktok-icon.png" alt="ícone do tiktok" />
-                        </a>
-                        <a href="">
-                            <img src="/icons/twitter-icon.png" alt="ícone do X" />
-                        </a>
+            <div className={styles.outer_container}>
+                <div className={styles.inner_container}>
+                    <div className={styles.intro_container}>
+                        <Link to="/">
+                            <img src={logo} />
+                        </Link>
+                        <div className={styles.social_container}>
+                            <a href="">
+                                <img src="/icons/facebook-icon.svg" alt="ícone do facebook" />
+                            </a>
+                            <a href="">
+                                <img src="/icons/instagram-icon.png" alt="ícone do instagram" />
+                            </a>
+                            <a href="">
+                                <img src="/icons/tiktok-icon.png" alt="ícone do tiktok" />
+                            </a>
+                            <a href="">
+                                <img src="/icons/twitter-icon.png" alt="ícone do X" />
+                            </a>
+                        </div>
                     </div>
+                    <p>De La Cocina es el sitio donde la pasión por la gastronomía argentina cobra vida. Descubre recetas auténticas, consejos prácticos y sabores que llevan la esencia de Argentina a tu mesa. ¡Aprovecha!</p>
                 </div>
-                <p>De La Cocina es el sitio donde la pasión por la gastronomía argentina cobra vida. Descubre recetas auténticas, consejos prácticos y sabores que llevan la esencia de Argentina a tu mesa. ¡Aprovecha!</p>
+                <div className={styles.topics_container}>
+                    <Link to="/">
+                        <a>Pagina de inicio</a>
+                    </Link>
+                    <Link to="/sobre">
+                        <a>Contacto</a>
+                    </Link>
+                    <Link to="/ingredientes">
+                        <a>Ingredientes</a>
+                    </Link>
+                    <Link to="/categorias">
+                        <a>Categorias</a>
+                    </Link>
+                </div>
             </div>
-            <div className={styles.topics_container}>
-                <Link to="/">
-                    <a>Pagina de inicio</a>
-                </Link>
-                <Link to="/sobre">
-                    <a>Contacto</a>
-                </Link>
-                <Link to="/ingredientes">
-                    <a>Ingredientes</a>
-                </Link>
-                <Link to="/categorias">
-                    <a>Categorias</a>
-                </Link>
-            </div>
+            <p>© 2024 De La Cocina</p>
         </footer>
     );
 }
